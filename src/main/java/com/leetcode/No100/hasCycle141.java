ac803ecb2aaf8041948a8071ca76774431b40fa2
liyi <18684997125@163.com>
2022-07-17 20:10:24 +0800

    算法训练营第一周：hasCycle141 环形链表

---
+++ src/main/java/com/leetcode/No100/hasCycle141.java
@@ -0,0 +1,52 @@
+package com.leetcode.No100;
+
+
+import com.leetcode.ListNode;
+
+/**
+ * 环形链表
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * <p>
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class hasCycle141 {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {// 这里fast.next一定要判断一下，因为如果链表只有一个元素[1]，肯定不是环形链表
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {// 在指针移动后再比较，排除初始都指向头结点的情况
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public boolean hasCycle_官方解法(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
